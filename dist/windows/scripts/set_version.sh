064071535351ba99f5972af3a2d69902839a27a5
Martin Bříza <m@rtinbriza.cz>
2019-10-24 16:31:16 +0200

    Echo status in the windows script

--- dist/windows/scripts/set_version.sh
+++ dist/windows/scripts/set_version.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+set -e
+set -x
+
 VERSION="$1"
 
 if [[ "$VERSION" == "" ]]; then
     exit 0
 fi
+
+echo $VERSION
 
 sed -i.bak "s/7\.0\.0\.0/$VERSION\.0/g" src/ui/windows/TogglDesktop/TogglDesktop/Properties/AssemblyInfo.cs
 sed -i.bak "s/7\.0\.0\.0/$VERSION\.0/g" src/ui/windows/TogglDesktop/TogglDesktop.Package/Package.appxmanifest
