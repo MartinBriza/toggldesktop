6aadd54f06337ee6c61e5cebafe77d44822c5c28
Martin Bříza <m@rtinbriza.cz>
2019-10-24 17:07:57 +0200

    odsngoasdbngoi

--- dist/tag-and-release.sh
+++ dist/tag-and-release.sh
@@ -52,10 +52,10 @@ fi
 curl -o /dev/null -sH "$AUTH" $GH_REPO || { echo "Error: Invalid repo, token or network issue!";  exit 1; }
 
 # and push the tag, that will make it discoverable later
-curl -d '{ "tag": "'$NEW_TAG'", "message": "'$NEW_TAG'",  "object": "'$CURRENT_COMMIT'", "type": "commit" }' -H "Content-Type: application/json" -X POST -o /dev/null -sH "$AUTH" $GH_RELEASES
+#curl -d '{ "tag": "'$NEW_TAG'", "message": "'$NEW_TAG'",  "object": "'$CURRENT_COMMIT'", "type": "commit" }' -H "Content-Type: application/json" -X POST -o /dev/null -sH "$AUTH" $GH_RELEASES
 
 # Create a release out of the tag (shouldn't fail if it's already released)
-curl -d '{ "tag_name": "'$NEW_TAG'", "target_commitish": "", "name": "'$NEW_TAG'", "body": "'$NEW_TAG'", "draft": false, "prerelease": true }' -H "Content-Type: application/json" -X POST -o /dev/null -sH "$AUTH" $GH_RELEASES
+curl -d '{ "tag_name": "'$NEW_TAG'", "target_commitish": "'$CURRENT_COMMIT'", "name": "'$NEW_TAG'", "body": "'$NEW_TAG'", "draft": false, "prerelease": true }' -H "Content-Type: application/json" -X POST -o /dev/null -sH "$AUTH" $GH_RELEASES
 
 # Read asset tags.
 response=$(curl -sH "$AUTH" $GH_TAG)
