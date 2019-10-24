2160a77f774bcf9447a607cd5ecdcc91532dd2db
Martin Bříza <m@rtinbriza.cz>
2019-10-24 16:43:11 +0200

    Still something wrong

--- dist/get-tag-name.sh
+++ dist/get-tag-name.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 set -e
+set -x
 
 PROBABLY_TAG=${GITHUB_REF/refs\/tags\//}
 
-[[ $PROBABLY_TAG =~ v[0-9][0-9]*[.][0-9][0-9]*[.][0-9][0-9]* ]] && echo $PROBABLY_TAG
+[[ "$PROBABLY_TAG" =~ v[0-9][0-9]*[.][0-9][0-9]*[.][0-9][0-9]* ]] && echo $PROBABLY_TAG
