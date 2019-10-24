7048047167a5ee2f207fec691f1dff42c6f60426
Martin Bříza <m@rtinbriza.cz>
2019-10-24 16:55:43 +0200

    Starting to get real desperate in here

--- dist/get-tag-name.sh
+++ dist/get-tag-name.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -x
-
 PROBABLY_TAG=${GITHUB_REF/refs\/tags\//}
 
-grep -Eq "^v[0-9][0-9]*[.][0-9][0-9]*[.][0-9][0-9]*$" <<< "$PROBABLY_TAG" && echo $PROBABLY_TAG
+grep "^v[0-9][0-9]*[.][0-9][0-9]*[.][0-9][0-9]*$" <<< "$PROBABLY_TAG"
